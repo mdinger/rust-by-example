7960ef539039ed604091ea4434084d014c1613e3
mdinger <mdinger.bugzilla@gmail.com>
2015-05-12 19:13:43 -0400

    wip

--- examples/generics/assoc_types/constrain/constrain.rs
+++ examples/generics/assoc_types/constrain/constrain.rs
@@ -1,6 +1,5 @@
 use std::fmt::Display;
 
-#[derive(Copy, Clone)]
 struct Container<T>(T);
 
 trait Contains {
@@ -9,41 +8,32 @@ trait Contains {
     fn inner(&self) -> Self::A;
 }
 
-impl<T: Copy> Contains for Container<T> {
+impl<T: Clone> Contains for Container<T> {
     type A = T;
 
     fn inner(&self) -> Self::A {
-        let Container(i) = *self;
+        let Container(ref i) = *self;
 
-        i
+        i.clone()
     }
 }
 
-fn printer_longform<C>(c: C) where
+// Bounds on associated types.
+fn printer<C>(c: C) where
     C: Contains,
     C::A: Display {
     println!("{}", c.inner());
 }
 
-fn num<C>(c: C) -> i32 where
-// Shorthand for this (which isn't supported yet anyway):
-// C: Contains,
-// C::A = i32
+// The assignment shorthand:
+fn num_small<C>(c: C) -> i32 where
     C: Contains<A = i32> {
     c.inner()
 }
-/*
-fn printer_compact<C>(c: C) where
-    C: Contains<A: Display> {
-    println!("{}", c.inner());
-}*/
+
 
 fn main() {
-    let i = Container(32);
-    let f = Container(64.0);
-    
-    printer_longform(i);
-    //printer_compact(f);
-    
-    println!("{}", num(i));
+    printer(Container(17i32));
+
+    println!("{}", num_small(Container(4i32)));
 }
